658cd699e69112887ad00e744887118198e136a1
Brijal kansara <brijalk@solguruz.com>
2025-07-14 13:48:59 +0530

    feat: add milky white fonts

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='h-20 backdrop-blur-[100px]'>
+    <div className="h-20 backdrop-blur-[100px] text-white font-milkwhite">
       Navbar
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
