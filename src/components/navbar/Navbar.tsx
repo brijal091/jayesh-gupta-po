7689daabed2b7a8b6a0bda7d9444d304d58c13de
Brijal kansara <brijalk@solguruz.com>
2025-07-14 15:42:55 +0530

    fix: add responsove menu items

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { NAME } from "@/constants/constants";
+import { NAME, navItems } from "@/constants/constants";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
